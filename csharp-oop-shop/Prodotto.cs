2f19897b6cb21f10dc15156b1571c4acb86a2107
MaxPredathor <max.gilli98@gmail.com>
2024-04-19 15:30:32 +0200

    Aggiungere i file di progetto.

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    public class Prodotto
+    {
+        public int Codice { get; }
+        public string Nome { get; set; }
+        public string Descrizione { get; set; }
+        public double Prezzo { get; set; }
+        public double Iva {  get; set; }
+
+        public Prodotto(string nome, string descrizione, double prezzo, double iva)
+        {
+            Random random = new Random();
+            Codice = random.Next(1, 1000000);
+
+            Nome = nome;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+            Iva = iva;
+        }
+        public double PrezzoBase()
+        {
+            return Prezzo;
+        }
+
+        public double PrezzoComprensivoDiIva()
+        {
+            return Prezzo * (1 + Iva);
+        }
+
+        public string PrezzoConCodice()
+        {
+            return Convert.ToString(this.Codice) + " " + this.Nome;
+        }
+
+        public string CodiceConPad()
+        {
+            if (Convert.ToString(this.Codice).Length >= 8)
+            {
+                return Convert.ToString(this.Codice);
+            }
+            else
+            {
+                int pad = 8 - Convert.ToString(this.Codice).Length;
+                string codiceConPad = Convert.ToString(this.Codice);
+                for (int i = 0; i < pad; i++)
+                {
+                    codiceConPad = "0" + codiceConPad;
+                }
+                return codiceConPad;
+            }
+        }
+    }
+}
---
+++ csharp-oop-shop/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace csharp_oop_shop
+{
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Prodotto prodotto1 = new Prodotto("Pane", "Molto croccante", 12.95, 0.22);
+            Console.WriteLine($"Codice: {prodotto1.Codice}, Nome: {prodotto1.Nome}, Descrizione: {prodotto1.Descrizione}, Prezzo: {prodotto1.Prezzo}, IVA: {prodotto1.Iva}");
+            double prezzoNormale = prodotto1.PrezzoBase();
+            Console.WriteLine($"Prezzo base: {prezzoNormale}");
+            double prezzoConIva = prodotto1.PrezzoComprensivoDiIva();
+            Console.WriteLine($"Prezzo con IVA: {prezzoConIva}");
+            Console.WriteLine(prodotto1.PrezzoConCodice());
+            Console.WriteLine(prodotto1.CodiceConPad());
+            Prodotto[] nuoviProdotti =
+            {
+                new Prodotto ("Latte", "Fresco e genuino", 15.50, 0.4),
+                new Prodotto ("Pasta", "Grano saraceno", 10.00, 0.4),
+                new Prodotto ("Coca-Cola", "Bevanda da 1L", 8.50, 0.9),
+                new Prodotto ("Batterie", "AAA", 20.50, 0.22),
+                new Prodotto ("Spremiagrumi", "Spreme gli agrumi", 60.75, 0.22),
+            };
+            int counter = 1;
+            foreach (Prodotto prodotto in nuoviProdotti)
+            {
+
+                Console.WriteLine($"\n\nProdotto n {counter}/{nuoviProdotti.Length}");
+                Console.WriteLine($"\nCodice: {prodotto.Codice}, \nNome: {prodotto.Nome}, \nDescrizione: {prodotto.Descrizione}, \nPrezzo: {prodotto.Prezzo} Euro, \nIVA: {prodotto.Iva}");
+                counter++;
+            }
+        }
+    }
+}
